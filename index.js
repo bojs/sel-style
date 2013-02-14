96654cf919066bc097fba1849bb6877348ef59f9
Pau Fracés <pfraces@gmail.com>
2013-02-14 22:26:23 +0100

    refactored as sel plugin

--- index.js
+++ index.js
@@ -1,66 +1,40 @@
-var sel = require('sel');
-
 module.exports = style;
 
-var style = function (selector) {
-  return new Style(selector);
-};
-
-var Style = function (selector) {
-  this.nodes = toArray(sel(selector));
-};
-
-Style.prototype.pos = function (x, y, relative) {
-  this.nodes.forEach(function (node) {
+var style = {
+  pos: function (args) {
+    var node = args.node;
     node.style.position = 'absolute';
-    node.style.left = x; 
-    node.style.top = y;
+    node.style.left = args.x; 
+    node.style.top = args.y;
 
-    if (relative) {
+    if (args.relative) {
       node.parentNode.style.position = 'relative';
     }
-  });
-  return this;
-};
-
-Style.prototype.size = function (width, height) {
-  this.nodes.forEach(function (node) {
-    node.style.width = width;
-    node.style.height = height;
-  });
-  return this;
-};
-
-Style.prototype.color = function (opts) {
-  this.nodes.forEach(function (node) {
-    if (opts.bg) {
-      node.style.background = opts.bg;
+  },
+  size: function (args) {
+    var node = args.node;
+    node.style.width = args.width;
+    node.style.height = args.height;
+  },
+  color: function (args) {
+    var node = args.node;
+    if (args.bg) {
+      node.style.background = args.bg;
     }
-    if (opts.fg) {
-      node.style.color = opts.fg;
+    if (args.fg) {
+      node.style.color = args.fg;
     }
-    if (opts.opacity) {
-      node.style.opacity = opts.opacity;
+    if (args.opacity) {
+      node.style.opacity = args.opacity;
     }
-  });
-  return this;
-};
+  },
+  add: function (args) {
+    args.node.className += ' ' + args.className;
+  },
+  del: function (args) {
+    var node = args.node,
+        regex = new RegExp('\\b' + args.className + '\\b');
 
-Style.prototype.add = function (className) {
-  this.nodes.forEach(function (node) {
-    node.className += ' ' + className;
-  });
-  return this;
-};
-
-Stype.prototype.del = function (className) {
-  this.nodes.forEach(function (node) {
-    node.className = node.className.
-      replace(new RegExp('\\b' + className + '\\b'));
-  });
-  return this;
-};
-
-var toArray = function (obj) {
-  return obj.length ? Array.prototype.slice.call(obj) : [].concat(obj);
+    node.className = node.className.replace(regex);
+  }
 };
